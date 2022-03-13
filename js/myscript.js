94f621c924ab51c37070b1b7271b3c0378a6f519
Abhisek Joshi <nofursad@gmail.com>
2022-03-13 07:23:52 +0000

    Change the html using jQuery

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("h2").css("text-decoration", "underline"); //underlines all <h2> elements
+    // $("h2").addClass("underline"); adds the already existing css class to elements
+    // $("h2").removeClass("underline"); remove the already existing css class to elements
+    $("ul").css("border", "solid, 1px, #ccc") //adds a border to all lists
+    // $("ul").addClass("border"); adds the border class from css to elements
+    // $("ul").removeClass("border"); remove the border class from css to elements
+})
